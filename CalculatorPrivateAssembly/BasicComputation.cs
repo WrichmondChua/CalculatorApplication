d4dbe63672f52b120c938e5012ad01a168821f2c
Wrichmond Well A. Chua <59139222+WrichmondChua@users.noreply.github.com>
2023-02-04 20:58:33 +0800

    Add files via upload

---
+++ CalculatorPrivateAssembly/BasicComputation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CalculatorPrivateAssembly
+{
+    public class BasicComputation
+    {
+        public float Addition (float num1, float num2)
+        {
+            float answer = num1 + num2;
+            return answer;
+        }
+        public float Subtraction(float num1, float num2)
+        {
+            float answer = num1 - num2;
+            return answer;
+        }
+        public float Multiplication(float num1, float num2)
+        {
+            float answer = num1 * num2;
+            return answer;
+        }
+        public float Division(float num1, float num2)
+        {
+            float answer = num1 / num2;
+            return answer;
+        }
+
+    }
+}
